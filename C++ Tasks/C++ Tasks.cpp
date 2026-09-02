a8c32b909179cb5f07010faf5e19c488a0e7220a
Tigrica <tigricashaurma@gmail.com>
2026-09-02 14:45:36 +1000

    Добавьте файлы проекта.

---
+++ C++ Tasks/C++ Tasks.cpp
@@ -0,0 +1,59 @@
+﻿
+//KT1________________
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <execution>
+#include <random>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int numbers[32768];
+    for (int i = 0; i < 32768; ++i) numbers[i] = rand() % (-1000 - 1000 + 1) + -1000;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 32768; ++i) std::cout << numbers[i] << std::endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto t1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 32768; ++i) std::cout << numbers[i] << '\n';
+    end = std::chrono::high_resolution_clock::now();
+    auto t1e = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 32768; ++i) {
+        printf("%d\n", numbers[i]);
+
+    }
+    end = std::chrono::high_resolution_clock::now();
+    auto t2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+
+
+    int summOFALL = 0;
+    int& summ = summOFALL;
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 32768; ++i) {
+        summ = summ + numbers[i];
+    }
+    end = std::chrono::high_resolution_clock::now();
+    printf("%d", summ);
+    auto t3 = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    std::cout << std::endl;
+    std::cout << "Cout endl: " << t1 << std::endl;
+    std::cout << "Cout |n: " << t1e << std::endl;
+    std::cout << "printf: " << t2 << std::endl;
+    std::cout << "summmmmmm: " << t3 << std::endl;
+
+}
+
+//KT1=============================
+
+
---
+++ C++ all/C++ all.cpp
@@ -0,0 +1,70 @@
+﻿//конспект 
+
+
+
+//#include <iostream> было изначально
+//#include <execution>
+//#include <vector>
+//#include <chrono> для времени
+//#include <random> 
+
+/*
+int main()
+{
+
+	setlocale(LC_ALL, "Russian"); для того чтобы ру текст в консоли отображался корректно
+
+
+	auto start = std::chrono::high_resolution_clock::now();
+	что-то
+	auto end = std::chrono::high_resolution_clock::now();
+	auto t1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+	Можно использовать для замера времени
+
+	high_resolution_clock - в наносекундах; auto прописывается в инициализации, потом не нужно
+
+
+	for (int i = 0; i < n; ++i)  --- ;
+	for (int i = 0; i < n; ++i){
+	---
+	}
+
+
+	Медленный способ
+	std::cout << --- << std::endl;
+	std::cout << --- << '\n';
+
+	std::cout - вывести строку; std::endl - перенос (медленнее); '\n' - лучше это использовать(быстрее);
+
+	Быстрый способ
+	printf('тег', сообщение );
+	printf("%d\n", numbers[i]); с новой строки начинать всё
+	           ^
+	теги
+
+
+
+	Ссылки
+	int summOFALL = 0;
+	int& summ = summOFALL; Создание ссылок
+	   ^
+	
+
+	Массивы
+	не изменяемый размер
+	задаётся сразу
+	int numbers[32768]; пример
+	for (int i = 0; i < 32768; ++i) numbers[i] = rand() % (-1000 - 1000 + 1) + -1000; заполнить так можно
+
+
+
+
+
+
+
+
+
+
+
+}
+*/
